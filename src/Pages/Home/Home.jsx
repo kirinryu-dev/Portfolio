f2701299cbf85bbac353248522549a253577d78c
kirinryu.dev <kirinryu.dev@gmail.com>
2024-06-18 10:55:28 +0000

    updating for now

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,22 @@
+import Navbar from '../../Components/Navigationbar/Navbar';
+import './Home.css'
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className='Home_content'>
+                <div className='content_left'>
+                    yo
+                </div>
+
+                <div className='content_right'>
+                    ya
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Home;

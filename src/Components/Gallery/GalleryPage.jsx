1f2384dbe2729c02e6186bb59c517dec6b25ab01
kirinryu.dev <kirinryu.dev@gmail.com>
2024-06-20 12:03:18 +0000

    work on gallery and modal function

--- src/Components/Gallery/GalleryPage.jsx
+++ src/Components/Gallery/GalleryPage.jsx
@@ -2,13 +2,66 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 // import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Cardgallery from '../Card/CardGallery'
+
+
+// import custom css 
+
+import './GalleryPage.css';
 
 export default function FixedContainer() {
   return (
     <React.Fragment>
       <CssBaseline />
       <Container fixed>
-        Here be placed the gallery
+        <div className='Project_card'>
+          <div>
+            <Cardgallery />
+          </div>
+
+          <div>
+            <Cardgallery />
+          </div>
+
+          <div>
+            <Cardgallery />
+          </div>
+
+
+        </div>
+
+        <div className='Project_card'>
+          <div>
+            <Cardgallery />
+          </div>
+
+          <div>
+            <Cardgallery />
+          </div>
+
+          <div>
+            <Cardgallery />
+          </div>
+
+
+        </div>
+
+        <div className='Project_card'>
+          <div>
+            <Cardgallery />
+          </div>
+
+          <div>
+            <Cardgallery />
+          </div>
+
+          <div>
+            <Cardgallery />
+          </div>
+
+
+        </div>
+        
       </Container>
     </React.Fragment>
   );
--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -13,13 +13,15 @@ export default function galeryShowcase () {
             <div className='content_top'>
                 <h1>Project Showcase Gallery</h1>
                 <p>
-                 Quam libero deleniti ab atque ducimus ut enim eligendi cum vero alias voluptate illum magnam ea nemo, nostrum suscipit quas inventore expedita facilis saepe dolorem excepturi deserunt fuga est. Sed, dicta totam temporibus itaque accusantium ipsa libero et dolorum! Velit similique eos nemo facilis, repudiandae id inventore! Neque molestiae reiciendis voluptas id! Nisi et sapiente quos voluptatum tempora perspiciatis officia aliquam, quod omnis nostrum deserunt harum neque eos, inventore vitae cum ut molestias accusamus obcaecati ipsa! Voluptatem illo praesentium veritatis!
-
+                    I challenged myself with several personal projects to showcase my skills and abilities.
+                    This gallery highlights these projects. Click on any project to see a modal with its description and a link to the project.
                 </p>
             </div>
 
             <div className='content_bottom'>
+
                 <GalleryPage />
+                
             </div>
         </div>
     )
